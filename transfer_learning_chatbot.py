b7fe47cefb6bdf0d2e21405fb6ee25a820ab9fcc
Adrian Rubio-Punal <adrian.rubio.punal@gmail.com>
2024-10-26 15:25:04 +0200

    Auto commit on sáb 26 oct 2024 15:25:04 CEST

--- transfer_learning_chatbot.py
+++ transfer_learning_chatbot.py
@@ -1,4 +1,18 @@
 import torch
 torch.cuda.empty.cache()
 import torch.nn as nn
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from datasets import load_dataset
+from torch.utils.data import DataLoader, Dataset
+import numpy as np
+import matpotlib.pyplot as plt
+from datetime import datetime
+
+# Load DailyDialog dataset
+dataset = load_dataset("daily_dialog")
+
+# Load the pre-trained OpenAssistant model and tokenizer
+model_name = "OpenAssistant/oasst-sft-1-pythia-12b"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
 
