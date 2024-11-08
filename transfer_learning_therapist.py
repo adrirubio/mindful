740a9695be03872158ad095728e4a7add992623e
Adrian Rubio-Punal <adrian.rubio.punal@gmail.com>
2024-11-08 21:00:04 +0100

    Auto commit on vie 08 nov 2024 21:00:04 CET

--- transfer_learning_therapist.py
+++ transfer_learning_therapist.py
@@ -18,3 +18,6 @@ chatbot_model.load_state_dict(torch.load(chatbot_model_path))
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 chatbot_model.to(device)
 chatbot_model.train() # Set to training mode
+
+# Load the therapy dataset
+dataset = load_dataset("Amod/mental_health_counseling_conversations")
