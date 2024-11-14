165a79deb4730ee355876f5307fc5386ade876a4
Adrian Rubio-Punal <adrian.rubio.punal@gmail.com>
2024-11-14 20:45:04 +0100

    Auto commit on jue 14 nov 2024 20:45:04 CET

--- transfer_learning_therapist.py
+++ transfer_learning_therapist.py
@@ -11,7 +11,7 @@ from datetime import datetime
 # Load chatbot tokenizer and model
 # chatbot_tokenizer_path = "facebook/opt-350m"
 # chatbot_model_path = "/home/adrian/Documents/Perceptron/model_weights/transfer_learning_chatbot.pth"
-# chatbot_tokenizer = AutoTokenizer.from_pretrained(chatbot_tokenizer_path)
+chatbot_tokenizer = AutoTokenizer.from_pretrained(chatbot_tokenizer_path)
 # chatbot_model = AutoModelForCausalLM.from_pretrained("facebook/opt-350m")
 # chatbot_model.load_state_dict(torch.load(chatbot_model_path))
 
@@ -23,8 +23,40 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Load the mental health dataset
 dataset = load_dataset("Amod/mental_health_counseling_conversations")
 
-# Print some train and test examples
-print(dataset["train"][0])
-print(dataset["test"][0])
+class TherapyDataset(Dataset):
+    def __init__(self, dataset, tokenizer, max_length=512):
+        self.dataset = dataset
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+        # Divide the dataset into train and test
+        split = int(len(dataset) * 0.9)
+        if train:
+            self.dataset = dataset[:split]
+        else:
+            self.dataset = dataset[split:]
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        # Format the conversation
+        conversation = (f"Patient: {self.dataset[idx]['Context']}\n"
+                        f"Therapist: {self.dataset[idx]['Response']}")
+
+        # Tokenize
+        encodings = self.tokenize(
+            conversation,
+            truncation=True,
+            max_length=self.max_length,
+            padding="max_length",
+            return_tensors="pt"
+        )
+
+        return {k: v.squeeze(0) for k, v in encodings.items()}
+
+# Create train and test datasets using the class
+train_dataset = TherapyDataset(dataset, tokenizer, train=True)
+test_dataset = TherapyDataset(dataset, tokenizer, train=False)
 
 
