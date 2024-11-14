7d6484ddb40894d8b6b542f6e20fc5b0b44498d1
Adrian Rubio-Punal <adrian.rubio.punal@gmail.com>
2024-11-14 20:20:03 +0100

    Auto commit on jue 14 nov 2024 20:20:03 CET

--- transfer_learning_therapist.py
+++ transfer_learning_therapist.py
@@ -3,21 +3,22 @@ torch.cuda.empty_cache()
 import torch.nn as nn
 from torch.utils.data import DataLoader, Dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from datasets import load_dataset
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
 
 # Load chatbot tokenizer and model
-chatbot_tokenizer_path = "facebook/opt-350m"
-chatbot_model_path = "/home/adrian/Documents/Perceptron/model_weights/transfer_learning_chatbot.pth"
-chatbot_tokenizer = AutoTokenizer.from_pretrained(chatbot_tokenizer_path)
-chatbot_model = AutoModelForCausalLM.from_pretrained("facebook/opt-350m")
-chatbot_model.load_state_dict(torch.load(chatbot_model_path))
+# chatbot_tokenizer_path = "facebook/opt-350m"
+# chatbot_model_path = "/home/adrian/Documents/Perceptron/model_weights/transfer_learning_chatbot.pth"
+# chatbot_tokenizer = AutoTokenizer.from_pretrained(chatbot_tokenizer_path)
+# chatbot_model = AutoModelForCausalLM.from_pretrained("facebook/opt-350m")
+# chatbot_model.load_state_dict(torch.load(chatbot_model_path))
 
 # Prepare model for training
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-chatbot_model.to(device)
-chatbot_model.train() # Set to training mode
+# chatbot_model.to(device)
+# chatbot_model.train() # Set to training mode
 
 # Load the mental health dataset
 dataset = load_dataset("Amod/mental_health_counseling_conversations")
@@ -25,3 +26,5 @@ dataset = load_dataset("Amod/mental_health_counseling_conversations")
 # Print some train and test examples
 print(dataset["train"][0])
 print(dataset["test"][0])
+
+
