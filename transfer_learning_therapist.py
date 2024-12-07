047a1ba0611fbf2584eba40277619529e5a32cfd
Adrian Rubio-Punal <76663380+adrirubio@users.noreply.github.com>
2024-12-07 12:03:10 +0100

    Update transfer_learning_therapist.py

--- transfer_learning_therapist.py
+++ transfer_learning_therapist.py
@@ -8,6 +8,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
 
+### Temporary changes
+
 # Load chatbot tokenizer and model
 # chatbot_tokenizer_path = "facebook/opt-350m"
 # chatbot_model_path = "/home/adrian/Documents/Perceptron/model_weights/transfer_learning_chatbot.pth"
