dc6558726dc970c3f2b4d49000ca7e81056e2a71
Adrian Rubio-Punal <adrian.rubio.punal@gmail.com>
2025-01-18 20:25:04 +0100

    Auto commit on sáb 18 ene 2025 20:25:04 CET

--- model.py
+++ model.py
@@ -1,1 +1,12 @@
 # inference.py 
+import torch.nn as nn
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+# Load the trained weights
+model.load_state_dict(torch.load("transfer_learning_therapist.pth", map_location=torch.device('cpu')))
+model.eval()
+
+# Load the tokenizer and model
+tokenizer = AutoTokenizer.from_pretrained("facebook/opt-2.7b")
+
